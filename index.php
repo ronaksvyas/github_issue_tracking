c26972f5a7d5928482c7e3825d637cab667e5036
ronaksvyas <ronaksvyas@gmail.com>
2016-07-23 15:06:25 +0530

    First working code commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Github Issue Tracking</title>
+</head>
+<body>
+<form action="process.php">
+  <span>Github repository url:</span>
+  <input type="text" name="repo_url">
+</form>
+</body>
+</html>
+<?php
+//setting timezone
+	date_default_timezone_set('UTC');
+
+?>
